a96e7c6100eec7cbb105ef62b96380fa182ea2db
Paige Watson <paige.watson@outlook.com>
2018-03-27 12:44:56 -0700

    Add fizz for three

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -8,6 +8,10 @@ export default class fizzBuzz {
     calculate() {
         var returnValue=[];
         for (var currentNumber = this.lowerBound; currentNumber <= this.upperBound; currentNumber++) {
+            if (currentNumber === 3) {
+                returnValue.push("fizz");
+                continue;
+            }
             returnValue.push(currentNumber.toString());
         }
         return returnValue;
--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -1,20 +1,29 @@
 import FizzBuzz from "./fizz-buzz";
 var assert = require('assert');
 
-it('Should Return int 1 as string',
+it('Should 1,2 Return int 1 as string',
     () => {
         //start here
         let fbObj = new FizzBuzz(1,1);
         var returnValue = fbObj.calculate();
         assert.deepEqual(["1"], returnValue);
     });
-it('Should Return string array [1,2]',
+it('Should 1,2 Return string array [1,2]',
     () => {
         //start here
         let fbObj = new FizzBuzz(1,2);
         var returnValue = fbObj.calculate();
         assert.deepEqual(["1","2"], returnValue);
     });
+
+it('Should 1,3 Return string array [1,2,fizz]',
+    () => {
+        //start here
+        let fbObj = new FizzBuzz(1,3);
+        var returnValue = fbObj.calculate();
+        assert.deepEqual(["1","2","fizz"], returnValue);
+    });
+
 
 
 
