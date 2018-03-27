1b1e5ab2acc8f3a43dd0756bff3d16bee8c86a0f
Paige Watson <paige.watson@outlook.com>
2018-03-27 12:59:44 -0700

    Refactor to remove duplicate code

--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -1,29 +1,15 @@
 import FizzBuzz from "./fizz-buzz";
 var assert = require('assert');
 
-it('Should 1,2 Return int 1 as string',
+it('Should Return int as string',
     () => {
         //start here
         let fbObj = new FizzBuzz(1,1);
         var returnValue = fbObj.calculate();
         assert.deepEqual(["1"], returnValue);
     });
-it('Should 1,2 Return string array [1,2]',
-    () => {
-        //start here
-        let fbObj = new FizzBuzz(1,2);
-        var returnValue = fbObj.calculate();
-        assert.deepEqual(["1","2"], returnValue);
-    });
 
-it('Should 1,5 Return string array [1,2,fizz,4,buzz]',
-    () => {
-        //start here
-        let fbObj = new FizzBuzz(1,5);
-        var returnValue = fbObj.calculate();
-        assert.deepEqual(["1","2","fizz","4","buzz"], returnValue);
-    });
-it('Should 1,6 Return string array [1,2,fizz,4,buzz,fizz]',
+it('Should Return fizz for multiple of 3',
     () => {
         //start here
         let fbObj = new FizzBuzz(1,6);
@@ -31,7 +17,7 @@ it('Should 1,6 Return string array [1,2,fizz,4,buzz,fizz]',
         assert.deepEqual(["1","2","fizz","4","buzz","fizz"], returnValue);
     });
 
-it('Should 1,10 Return string array [1,2,fizz,4,buzz,fizz,6,7,8,fizz,buzz]',
+it('Should Return buzz for multiple of 5',
     () => {
         //start here
         let fbObj = new FizzBuzz(1,10);
