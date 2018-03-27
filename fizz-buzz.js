24f6e8d46d7db8ed649d3e9c38dc0883f730435d
Paige Watson <paige.watson@outlook.com>
2018-03-27 12:39:36 -0700

    Now returning array as required.  Also, first two tests written.

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,11 +1,16 @@
 export default class fizzBuzz {
 
-    constructor(numberToConvert) {
-        this.numberToConvert = numberToConvert;
+    constructor(lowerBound, upperBound) {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
     }
     
     calculate() {
-        return '1';
+        var returnValue=[];
+        for (var currentNumber = this.lowerBound; currentNumber <= this.upperBound; currentNumber++) {
+            returnValue.push(currentNumber.toString());
+        }
+        return returnValue;
     }
     
     
--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -4,12 +4,17 @@ var assert = require('assert');
 it('Should Return int 1 as string',
     () => {
         //start here
-        let fbObj = new FizzBuzz(1);
+        let fbObj = new FizzBuzz(1,1);
         var returnValue = fbObj.calculate();
-        assert.equal('1', returnValue);
-
+        assert.deepEqual(["1"], returnValue);
     });
-
+it('Should Return string array [1,2]',
+    () => {
+        //start here
+        let fbObj = new FizzBuzz(1,2);
+        var returnValue = fbObj.calculate();
+        assert.deepEqual(["1","2"], returnValue);
+    });
 
 
 
